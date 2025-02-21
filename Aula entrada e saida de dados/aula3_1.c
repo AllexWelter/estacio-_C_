44e35313164239b8d6f4057b61843ccded1d78c2
Allex Welter <allexwelter13@gmail.com>
2025-02-21 15:35:40 -0300

    .

--- Aula entrada e saida de dados/aula3_1.c
+++ Aula entrada e saida de dados/aula3_1.c
@@ -12,6 +12,13 @@ int main(){
 
     printf("Digite sua idade: ");
     scanf("%d", &idade);
-    printf("A idade e: %d", idade);
+    printf("A idade e: %d\n", idade);
 
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+    printf("A altura e: %.2f\n", altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome); //nao precisou do & pois trata-se de string 
+    printf("Seu nome e: %s\n", nome);
 }
