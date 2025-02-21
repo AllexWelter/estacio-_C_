13dd50acd20a49ce669fea74f5a47cb0ebae38d3
Allex Welter <allexwelter13@gmail.com>
2025-02-21 16:01:30 -0300

    .

--- Aula entrada e saida de dados/aula3_1.c
+++ Aula entrada e saida de dados/aula3_1.c
@@ -21,4 +21,9 @@ int main(){
     printf("Digite seu nome: ");
     scanf("%s", nome); //nao precisou do & pois trata-se de string 
     printf("Seu nome é: %s\n", nome);
+
+
+    printf("Digite a opção: ");
+    scanf(" %c", &opcao); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+    printf("A opção é: %c\n", opcao);
 }
