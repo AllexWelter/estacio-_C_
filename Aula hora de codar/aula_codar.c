1c6cad88b318df22e98158377454bb466b278ca4
Allex Welter <allexwelter13@gmail.com>
2025-02-22 07:04:27 -0300

    .

--- Aula hora de codar/aula_codar.c
+++ Aula hora de codar/aula_codar.c
@@ -3,5 +3,23 @@
 
 int main() {
 
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
     
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", nome);
+
+    printf("Digite sua matrícula: ");
+    scanf("%d", &matricula);
+
+    printf("O aluno %s tem %d anos de idade e %.2f de altura, possuindo a matrícula de número %d.\n", nome, idade, altura, matricula);
+
+    return 0;
 }
