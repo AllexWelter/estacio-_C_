d7d0b3952e6b5c8cbf01be22cf54ef1599962d06
Allex Welter <allexwelter13@gmail.com>
2025-02-24 17:09:13 -0300

    .

---
+++ Nível Aventureiro/Desafio nivel aventureiro/super_trunfo_1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+
+int main() {
+    //declaração de variáveis se valor
+    char estado;
+    char codigo[10];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+    
+
+    //solicitação inserção de dados do usuário e leitura dos dados digitados para Carta A
+    printf("Digite uma letra para o Estado: \n");
+    scanf("%c", &estado);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", codigo); //nao precisou do & pois trata-se de string 
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", cidade); //nao precisou do & pois trata-se de string 
+
+    printf("Digite a população da cidade: \n");
+    scanf(" %d", &populacao);
+
+    printf("Digite a área da cidade: \n");
+    scanf(" %f", &area); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    printf("Digite o PIB da cidade: \n");
+    scanf(" %f", &pib); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    printf("Digite a quantidade de pontos turísticos da cidade: \n");
+    scanf(" %d", &pontosTuristicos); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    float densidadePopulacional = (float) populacao / area;
+    float pibPerCapita =  (pib * 1e9) / (float) populacao;  //convertendo pib para reais -> 699,28 X 1.000.000.000 = 699.280.000.000 
+
+    //Saída do resultado na tela com as variáveis com seus devidos valores para Carta A.
+    printf("Carta 1:\n Estado: %c\n Código: %s\n Nome da Cidade: %s\n População: %d\n Àrea: %.2fkm²\n PIB: %.2f bilhões de reais\n Número de Pontos Turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado, codigo, cidade, populacao, area, pib, pontosTuristicos, densidadePopulacional, pibPerCapita);
+
+    
+    //solicitação inserção de dados do usuário e leitura dos dados digitados para Carta B
+    printf("Digite uma letra para o Estado: \n");
+    scanf(" %c", &estado); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", codigo); //nao precisou do & pois trata-se de string 
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", cidade); //nao precisou do & pois trata-se de string 
+
+    printf("Digite a população da cidade: \n");
+    scanf(" %d", &populacao);
+
+    printf("Digite a área da cidade: \n");
+    scanf(" %f", &area); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    printf("Digite o PIB da cidade: \n");
+    scanf(" %f", &pib); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    printf("Digite a quantidade de pontos turísticos da cidade: \n");
+    scanf(" %d", &pontosTuristicos); // se não inserir um espaço antes do formato nessa situação, o comando não será lido corretamente.
+
+    float densidadePopulacional1 = (float) populacao / area;
+    float pibPerCapita1 =  (pib * 1e9) / (float) populacao; //convertendo pib para reais -> 699,28 X 1.000.000.000 = 699.280.000.000 
+
+    //Saída do resultado na tela com as variáveis com seus devidos valores para Carta B.
+    printf("Carta 2:\n Estado: %c\n Código: %s\n Nome da Cidade: %s\n População: %d\n Àrea: %.2fkm²\n PIB: %.2f bilhões de reais\n Número de Pontos Turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado, codigo, cidade, populacao, area, pib, pontosTuristicos, densidadePopulacional1, pibPerCapita1);
+
+    return 0; //finalização correta do programa.
+
+}
+
+
+// https://github.com/Cursos-TI/desafio-cadastro-das-cartas-no-super-trunfo-AllexWelter
