86d0431ad9b57e81ee4fe288a7020df485fa6ab1
Allex Welter <allexwelter13@gmail.com>
2025-02-24 08:33:42 -0300

    .

--- Nível Aventureiro/Aula operadores matematicos/aula3_2.c
+++ Nível Aventureiro/Aula operadores matematicos/aula3_2.c
@@ -11,13 +11,28 @@ int main(){
     Pós-Decremento a--
     */
 
-    int numero1 = 1;
+    int numero1 = 1, resultado;
     
     printf("Antes incremento: %d\n", numero1);
     //numero1 = numero1 + 1
-    numero1++;
-    printf("Após incremento: %d\n", numero1);
+    //numero1 =+ 1;
+    //pos incremento: resultado = numero1 e depois numero++ faz o incremento
+    resultado = numero1++;
+    // printf("Após incremento: %d\n", numero1);
+    printf("Após Pós-incremento - Numero 1 : %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = ++numero1;
+    printf("Após Pré-incremento - Numero 1 : %d - Resultado: %d\n", numero1, resultado);
+
+
     //numero1 = numero1 - 1
-    numero1--;
-    printf("Após decremento: %d\n", numero1);
+    //numero1 -= 1
+    // numero1--;
+    // printf("Após decremento: %d\n", numero1);
+
+    resultado = numero1--;
+    printf("Após Pós-decremento - Numero 1 : %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Após Pré-decremento - Numero 1 : %d - Resultado: %d\n", numero1, resultado);
 }
