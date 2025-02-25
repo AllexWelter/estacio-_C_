ec22df31b95eb8aa2434931fcf33de1e77dba864
Allex Welter <allexwelter13@gmail.com>
2025-02-25 06:02:16 -0300

    .

--- Nível Mestre/Aula modificadores de tipos de dados/aula1.c
+++ Nível Mestre/Aula modificadores de tipos de dados/aula1.c
@@ -1,2 +1,18 @@
-// unsigned, signed, long, short
+//modificadores de tipos de dados ---> unsigned, signed, long, short
+
+
+// UNSIGNED
+#include <stdio.h>
+
+int main() {
+
+    int numeroSinal = 3000000000; //este valor excede o limite de um int normal
+    unsigned int numeroSemSinal = 3000000000;
+
+    printf("Número com sinal: %d\n", numeroSinal);
+    printf("Número sem sinal: %u\n", numeroSemSinal);
+
+    return 0;
+
+}
 
