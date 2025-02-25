a504a13af82cc195756cebc831e3df5625554bec
Allex Welter <allexwelter13@gmail.com>
2025-02-25 06:24:24 -0300

    .

--- Nível Mestre/Aula modificadores de tipos de dados/aula1.dSYM/Contents/aula1_1.c
+++ Nível Mestre/Aula modificadores de tipos de dados/aula1.dSYM/Contents/aula1_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-
+//LONG
 int main(){
 
+    int numerNormal = 2147483647; //valor máximo de int
+    long int numeroGrande = 2147483647;
+
+    printf("Número regular(int): %d\n", numerNormal);
+    printf("Número grande (long int): %ld\n", numeroGrande);
+
+    numeroGrande = 2147483648; //valor maior que o máximo de int
+    printf("Número grande atualizado (long int): %ld\n", numeroGrande);
+
+    return 0;
     
+
 }
