25896a7b67adcb1cb44ed85ffd9c60babe7ad9fb
Allex Welter <allexwelter13@gmail.com>
2025-02-28 07:12:25 -0300

    .

--- Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula operadores lógicos/aula1_2.c
+++ Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula operadores lógicos/aula1_2.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
 
+// int main(){
+
+//     int a = 0;
+
+//     if(!a) {
+//         printf("Variável A é zero.\n");
+//     } else {
+//         printf("A variável é diferente de zero.\n");
+//     }
+
+//     return 0;
+// }
+
 int main(){
 
-    int a = 0;
+    int a = -10;
 
-    if(!a) {
-        printf("Variável A é zero.\n");
+    if(!(a > 0)) {
+        printf("Variável é negativa.\n");
     } else {
-        printf("A não é um número positivo.\n");
+        printf("A variável é positiva.\n");
     }
 
     return 0;
 }
+
+
+
+// em C, tudo que é ZERO é falso e tudo que é diferente de ZERO é verdadeiro 
---
+++ Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula operadores lógicos/aula1_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int idade = 20;
+    float altura = 1.75;
+
+    // idade >= 18 => Verdadeiro
+    // idade <= 30 => Verdadeiro
+    // Verdadeiro && Verdadeiro
+    // Verdadeiro && altura > 1.70
+    // Verdadeiro && Verdadeiro => Verdadeiro 
+
+    if (idade >= 18 && idade <= 30 && altura > 1.70) {
+        printf("Você está na faixa etário e tem a a altura adequada.\n");
+    } else {
+        printf("Você não atende aos critérios.\n");
+    }
+        
+    return 0;
+}
