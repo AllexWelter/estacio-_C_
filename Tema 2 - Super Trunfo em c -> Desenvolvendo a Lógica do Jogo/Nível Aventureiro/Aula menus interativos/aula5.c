6fe94e72421040cce71e4f96e3cad36dd2893c7d
Allex Welter <allexwelter13@gmail.com>
2025-03-02 08:27:02 -0300

    .

--- Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula menus interativos/aula5.c
+++ Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula menus interativos/aula5.c
@@ -17,8 +17,8 @@ int main(){
     {
     case 1:  
         srand(time(0));
-        numeroSecreto = rand() % 10;
-        printf("Digite um número de 0 a 9: ");
+        numeroSecreto = rand() % 10 + 1;
+        printf("Digite um número de 0 a 10: ");
         scanf("%d", &palpite);
         if(numeroSecreto == palpite)
         {
@@ -31,7 +31,7 @@ int main(){
         
         break;
     case 2:
-        printf("A explicação das regras!\n");
+        printf("A explicação das regras do jogo!\n");
         printf("Digite a opção relacionada as regras do jogo: \n");
         scanf("%d", &regras);
         switch (regras)
@@ -51,10 +51,56 @@ int main(){
         printf("Saindo do jogo... \n");
         break;
     default:
-        printf("Opção inválida\n");
+        printf("Opção inválida, tente novamente!\n");
         break;
     }
 
 
     return 0;
 }
+
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+// int main() {
+//   int opcao;
+//   int numeroSecreto, palpite;
+
+//   printf("Menu Principal\n");
+//   printf("1. Iniciar Jogo\n");
+//   printf("2. Ver Regras\n");
+//   printf("3. Sair\n");
+//   printf("Escolha uma opção: \n");
+//   scanf("%d", &opcao);
+
+//   switch (opcao) {
+//     case 1:
+//       srand(time(0));
+//       numeroSecreto = rand() % 10 + 1;
+//       printf("Adivinhe o número (entre 1 e 10): \n");
+//       scanf("%d", &palpite);
+
+//       if (palpite == numeroSecreto) {
+//         printf("Parabéns! Você acertou!\n");
+//       } else {
+//         printf("Você errou. O número era %d.\n", numeroSecreto);
+//       }
+//       break;
+//     case 2:
+//       printf("Regras do Jogo:\n");
+//       printf("1. Escolha uma opção no menu.\n");
+//       printf("2. Se você escolher 'Iniciar Jogo', adivinhe o número secreto.\n");
+//       printf("3. O jogo termina quando você escolhe 'Sair'.\n");
+//       break;
+//     case 3:
+//       printf("Saindo...\n");
+//       break;
+//     default:
+//       printf("Opção inválida. Tente novamente.\n");
+//   }
+
+//   return 0;
+// }
