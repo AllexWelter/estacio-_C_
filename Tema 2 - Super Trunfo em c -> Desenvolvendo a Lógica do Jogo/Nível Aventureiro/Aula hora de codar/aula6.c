6c2099fb45621164038505ec86d6bc0630a581c8
Allex Welter <allexwelter13@gmail.com>
2025-03-03 06:18:51 -0300

    Update aula6.c

---
+++ Tema 2 - Super Trunfo em c -> Desenvolvendo a Lógica do Jogo/Nível Aventureiro/Aula hora de codar/aula6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int escolhaJogador, escolhaComputador;
+    srand(time(0));
+
+    printf("***JOGO DE JOKENPÔ***\n");
+    printf("Escolha uma opção: \n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tesoura\n");
+    scanf("%d", escolhaJogador);
+
+
+
+
+    return 0;
+
+}
