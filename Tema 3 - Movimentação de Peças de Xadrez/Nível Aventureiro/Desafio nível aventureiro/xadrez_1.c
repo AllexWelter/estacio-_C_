fdbfd4327feb993728097e9a1a59423cfd7a6661
Allex Welter <allexwelter13@gmail.com>
2025-03-11 08:12:28 -0300

    .

---
+++ Tema 3 - Movimentação de Peças de Xadrez/Nível Aventureiro/Desafio nível aventureiro/xadrez_1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int bispo = 1;
+    int rainha = 1;
+    int cavalo;
+    
+    printf("\nBispo:\n");  //Imprime o nome da peça a ser movimentada
+    while (bispo <= 5){ // mover diagonalmente o bispo 5 vezes
+        printf("Cima, Direita\n"); //Imprime a direção do movimento
+
+        bispo++;
+    }
+
+    printf("\nRainha:\n"); //Imprime o nome da peça a ser movimentada
+    do
+    {
+       printf("Esquerda\n");  //Imprime a direção do movimento
+       rainha++;
+
+    } while (rainha <= 8); // mover para a esquerda a rainha 8 vezes
+    
+    printf("\nTorre:\n"); //Imprime o nome da peça a ser movimentada
+    for(int torre = 1; torre <= 5; torre++) // mover a torre para a direita 5 vezes
+    {
+        printf("Direita\n");  //Imprime a direção do movimento
+    }
+
+    printf("\nCavalo:\n"); //Imprime o nome da peça a ser movimentada
+    for(cavalo = 1; cavalo <= 2; cavalo++){ // mover o cavalo para cima 2 vezes
+
+        printf("Cima\n"); //Imprime a direção do movimento
+        
+    } while (cavalo >= 2) // mover o cavalo para a direita 1 vez
+    {
+        printf("Direita\n"); //Imprime a direção do movimento
+        break;
+    }
+    
+
+
+    return 0;
+}
+
+
+// https://github.com/Cursos-TI/desafio-xadrez-AllexWelter
