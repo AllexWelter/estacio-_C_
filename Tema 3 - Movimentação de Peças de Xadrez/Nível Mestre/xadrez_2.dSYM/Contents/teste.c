e0852330b4b89a6155575395d4f6279daf8f1c84
Allex Welter <allexwelter13@gmail.com>
2025-03-22 15:05:53 -0300

    Update teste.c

--- Tema 3 - Movimentação de Peças de Xadrez/Nível Mestre/xadrez_2.dSYM/Contents/teste.c
+++ Tema 3 - Movimentação de Peças de Xadrez/Nível Mestre/xadrez_2.dSYM/Contents/teste.c
@@ -30,7 +30,6 @@ int main(){
 //     for(cont = 1; cont <= 20; cont++){
 //         scanf("%d", &num);
         
-
 //         if (controle == 1)
 //         menor = num;
 //         else if (num < menor)
